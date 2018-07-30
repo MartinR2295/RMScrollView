706cb72e3fa1537cd758638734b761ab168d756a
Martin Rader <mrader@anexia-it.com>
2018-07-30 14:36:42 +0200

    Initial Commit

---
+++ RMScrollView/RMScrollView.h
@@ -0,0 +1,19 @@
+//
+//  RMScrollView.h
+//  RMScrollView
+//
+//  Created by Martin Rader on 30.07.18.
+//  Copyright © 2018 Martin Rader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMScrollView.
+FOUNDATION_EXPORT double RMScrollViewVersionNumber;
+
+//! Project version string for RMScrollView.
+FOUNDATION_EXPORT const unsigned char RMScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMScrollView/PublicHeader.h>
+
+
